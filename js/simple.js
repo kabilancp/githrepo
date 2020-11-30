ad3bd4aeab0c4583ce5da3616e308efa9de8d3cb
kabilancp <kabilan.cp@gmail.com>
2020-11-30 19:45:06 +0530

    javascript code is added

---
+++ js/simple.js
@@ -0,0 +1,5 @@
+<script>
+	function windowopem(){
+		'/c/Users/COMPAQ/Downloads/y2mate.com - What is Git _  What is GitHub _  Git Tutorial  _ Github Tutorial  _ Beginners _ Tamil_1080p.mp4'}<script>
+	</script>
+	</script>
